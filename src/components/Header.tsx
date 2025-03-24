55fe3f22bbe84487cf40f1293d94aa36fe303346
nima boustanian <nima.boustanian@gmail.com>
2025-03-24 20:51:32 +0100

    added login link

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -2,7 +2,7 @@ import { Loader2, LogIn, LogOut } from "lucide-react";
 import { useAuth } from '../AuthContext';
 
 const Header: React.FC = () => {
-  const { logout, loading, isAuthenticated } = useAuth();
+  const { logout, loading, login, isAuthenticated } = useAuth();
 
   return (
     <header className="flex flex-wrap sm:justify-start sm:flex-nowrap w-full text-sm py-3">
@@ -31,7 +31,7 @@ const Header: React.FC = () => {
           {!isAuthenticated && !loading && (
             <button
               type="button"
-              onClick={() => console.log("Login not implemented")}
+              onClick={login}
               disabled={loading}
               className="py-2 px-4 inline-flex items-center gap-x-2 text-sm font-medium rounded-lg border border-transparent bg-teal-100 text-teal-800 hover:bg-teal-200 focus:outline-hidden focus:bg-teal-200 disabled:opacity-50 disabled:pointer-events-none dark:text-teal-500 dark:bg-teal-800/30 dark:hover:bg-teal-800/20 dark:focus:bg-teal-800/20"
             >
